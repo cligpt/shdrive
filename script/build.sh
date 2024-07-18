8807c38536fe771f4b743c81740c9cf59ddd4951
Jia <49358172+craftslab@users.noreply.github.com>
2024-07-18 11:52:50 +0800

    feat(script): add support to build windows & linux amd64 release

--- script/build.sh
+++ script/build.sh
@@ -10,4 +10,4 @@ go env -w GOPROXY=https://goproxy.cn,direct
 
 # go tool dist list
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "$ldflags" -o bin/$target main.go
-CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -ldflags "$ldflags" -o bin/$target main.go
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -ldflags "$ldflags" -o bin/$target.exe main.go
